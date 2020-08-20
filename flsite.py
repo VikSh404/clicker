1e09df7a4f97dabb88dd596ee9c35bf026553bc9
Victor Shevchenko <viktorshevchenko404@gmail.com>
2020-08-20 14:15:28 +0300

    rebuilded API branch, allowed cross for /api/

--- flsite.py
+++ flsite.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_cors import CORS
 import redis
 import os
+
+
+
 REDIS_PORT=6379
 HOST='redis'
 
 TEMPLATE_DIR = os.path.abspath('templates')
 STATIC_DIR = os.path.abspath('static')
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route("/index")
 @app.route("/")
@@ -14,15 +19,14 @@ def index():
     print(url_for('index'))
     return render_template('index.html', title="Clicker")
 
-@app.route("/reset")
-def reset():
-    #print(url_for('index'))
-    conn = redis.Redis(host=HOST, port=REDIS_PORT)
-    outValue=conn.delete('number')
-    return redirect(url_for('index'))
+# @app.route("/api/reset",  methods=["DELETE"]) # DELETE
+# def reset():
+#     conn = redis.Redis(host=HOST, port=REDIS_PORT)
+#     outValue=conn.delete('number')
+#     return str('Ok') #redirect(url_for('index'))
 
 
-@app.route("/api/number", methods=('GET', 'POST'))
+@app.route("/api/number", methods=["GET", "POST","DELETE"])
 def number():
     if request.method == 'GET':
         conn = redis.Redis(host=HOST, port=REDIS_PORT)
@@ -41,6 +45,10 @@ def number():
         else:
             outValue=outValue.decode('utf-8')
             conn.set('number', int(outValue)+1)
+    if request.method == 'DELETE':
+        conn = redis.Redis(host=HOST, port=REDIS_PORT)
+        outValue=conn.delete('number')
+
     return str(outValue)
 
 
